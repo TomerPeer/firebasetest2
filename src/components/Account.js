d31a65723c6c5ad8e4a275e7da89b722e26bd26d
TomerPeer <tomerpeer2002@gmail.com>
2018-04-07 14:25:26 +0300

    forget password and change password

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const Account = () =>
-    <div>
-        <h1>Account</h1>
-    </div>
+import AuthUserContext from './AuthUserContext';
+import { PasswordForgetLink } from './PasswordForget';
+import PasswordChangeForm from './PasswordChange';
 
-export default Account;
+const AccountPage = () =>
+    <AuthUserContext.Consumer>
+        {authUser =>
+            <div>
+                <h1>Account: {authUser.email}</h1>
+                <PasswordChangeForm />
+                <PasswordForgetLink />
+            </div>
+        }
+    </AuthUserContext.Consumer>
+
+export default AccountPage;
